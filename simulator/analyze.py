96d157a1ab9e7078fa94bccfb37f7d2b430c89f9
Marina Elmore <marina@dhcp-10-250-140-208.harvard.edu>
2022-11-09 16:55:55 -0500

    Adding simple analysis for pending / JCT time

--- simulator/analyze.py
+++ simulator/analyze.py
@@ -7,6 +7,9 @@ parser.add_argument("-lf", "--log_file", help="Log File")
 parser.add_argument("-jf", "--job_file", help="Job File")
 args = parser.parse_args()
 
+#Convert MS TO S
+TIME_CONST = 1000000000
+
 def read_into_pandas():
     if 'log_file' in args and args.log_file:
         file_path = "%s/job.csv" %args.log_file
@@ -29,13 +32,21 @@ def analyze_job_file():
     if df.empty:
         print("I should handle this error here and exit")
         exit(0)
+    else:
+        print(df)
 
-    print(df)
-    
     # Total Run Time
     start_time = df.iloc[0]['time']
     end_time = df.iloc[len(df)-1]['time']
-    print("*** Total Run Time: %s" %(end_time-start_time))
+    print("*** Total Run Time: %s" %((end_time-start_time)/TIME_CONST))
+
+    # Max Pending time
+    pending_time = df['pending_time']
+    print("*** Max Pending Time: %s" %(max(pending_time)/TIME_CONST))
+
+    # Max JCT
+    jct_time = df['JCT']
+    print("*** Max JCT: %s" %(max(jct_time)/TIME_CONST))
 
 if __name__ == '__main__':
     analyze_job_file()
